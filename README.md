94235b757c2d158d3803fd71f5135c661d69e658
chris (daiyi) <daiyitastic@gmail.com>
2018-01-27 23:46:18 -0800

    make a fake random endpoint

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Setup Instructions
 3. Create a new virtual environment (called "env") using `virtualenv -p [your path to python3] env`.
 4. Activate the virtual environment using `. env/bin/activate`
 5. Install Flask inside the virtual environment using `pip install Flask`
+6. `pip install -U flask-cors`
 6. To run the frontend, install npm. Suggestion: install npm using nvm.
 
 Development Instructions
--- growus/growus.py
+++ growus/growus.py
@@ -16,13 +16,30 @@ app.config.update(dict(
 app.config.from_envvar('GROWUS_SETTINGS', silent=True)
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from flask_cors import CORS
+import json
 
-app = Flask(__name__, static_folder="../static", template_folder="../templates")
+app = Flask(__name__, static_folder="../web", template_folder="../templates")
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    SECRET_KEY='ayyy',
+	CORS_HEADERS = 'Content-Type'
+))
+
+CORS(app)
 
 @app.route("/")
 def hello():
 	return "Let's grow us!"
+
+@app.route("/workout/random", methods = ['GET'])
+def api_workout_random():
+	workout = {"workout": ["squeeze doughnut", "stir-fry with cast iron pan", "ride bicycle!", "eat chocolate"]}
+	js = json.dumps(workout)
+	response = Response(js, status=200, mimetype="application/json")
+	return response
 
 if __name__ == "__main__":
 	app.run(port=9001)
