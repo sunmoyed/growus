c3fa720e1be0aedef91407d2ca48df145dfba287
chris (daiyi) <daiyitastic@gmail.com>
2018-01-29 17:06:54 -0800

    create fake api data

---
+++ growus/fake_data.py
@@ -0,0 +1,5 @@
+exercises = ["squeeze doughnut",
+             "stir-fry with cast iron pan",
+             "ride bicycle to ice cream shop",
+             "eat chocolate",
+             "hike to class through forest school"]
--- growus/growus.py
+++ growus/growus.py
@@ -20,6 +20,8 @@ from flask import Flask, render_template, Response
 from flask_cors import CORS
 import json
 
+import fake_data
+
 app = Flask(__name__, static_folder="../web", template_folder="../templates")
 app.config.from_object(__name__)
 
