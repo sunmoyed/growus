348722f263980424a0f7a76cd2877732640c43ac
chris (daiyi) <daiyitastic@gmail.com>
2018-01-31 23:55:27 -0800

    oops proxy doesn't work like that

--- web/src/RandomWorkout.js
+++ web/src/RandomWorkout.js
@@ -9,7 +9,7 @@ class RandomWorkout extends Workout {
   }
 
   fetchRandomWorkout() {
-    fetch('/workout/random')
+    fetch('http://localhost:9001/workout/random')
       .then(results => results.json())
       .then(jsonResponse => {
         this.setState({workout: jsonResponse.workout})
