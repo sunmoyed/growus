dea8fe7ae5ce010c1cec0d0229c42254269ae3c6
chris <daiyitastic@gmail.com>
2019-09-16 16:45:49 -0700

    there's no way these are actual secrets, this api is for web apps

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -4,7 +4,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "idk"
+  apiKey: "AIzaSyBxeS2P_8hW_RpNGhcAzq5kHhi3B6dVwM0",
+  authDomain: "grow-us.firebaseapp.com",
+  databaseURL: "https://grow-us.firebaseio.com",
+  projectId: "grow-us",
+  storageBucket: "",
+  messagingSenderId: "614348119405",
+  appId: "1:614348119405:web:392d641116df6d50cb4e64"
 };
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
