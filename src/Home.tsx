5f2555a86eb3efb26e3ff253faefea6e8f3b46a4
chris <daiyitastic@gmail.com>
2019-09-16 18:40:15 -0700

    show icon

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,26 +1,51 @@
 import React from "react";
 import { firebaseAuth } from "./Auth";
+import { User } from "firebase/app";
 
-const Home = ({ user }) => {
-  console.log(user);
-  return (
-    <div>
-      <p>
-        {user.displayName || "friend"} (
-        <button
-          className="link-button"
-          onClick={e => {
-            e.preventDefault();
-            firebaseAuth().signOut();
-          }}
-        >
-          logout
-        </button>
-        )
-      </p>
-      <p>you got this. \o/</p>
-    </div>
-  );
+type Props = {
+  user: User;
 };
 
-export default Home;
+type State = {
+  input: string;
+};
+
+export default class Home extends React.PureComponent<Props, State> {
+  state = {
+    input: ""
+  };
+
+  handleInputChange = input => {
+    this.setState({ input });
+  };
+
+  render() {
+    const { input } = this.state;
+    const { user } = this.props;
+
+    console.log(user);
+    return (
+      <div>
+        <p>
+          <img
+            className="user-icon"
+            src={user.photoURL || undefined}
+            alt="user icon"
+          />{" "}
+          {user.displayName || "friend"} (
+          <button
+            className="link-button"
+            onClick={e => {
+              e.preventDefault();
+              firebaseAuth().signOut();
+            }}
+          >
+            logout
+          </button>
+          )
+        </p>
+        <p>you got this. \o/</p>
+      </div>
+    );
+  }
+}
