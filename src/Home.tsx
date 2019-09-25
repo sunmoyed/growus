b3bd37550fdf11dad1371c430245377fd729578b
chris <daiyitastic@gmail.com>
2019-09-25 06:54:14 -0700

    make separate pages with history navigation

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,16 +6,89 @@ import Encouragements, {
 } from "./Encouragement";
 
 type Props = ProfileProps;
+type State = {
+  page: string;
+};
 
-export default class Home extends React.PureComponent<Props> {
+export default class Home extends React.Component<Props, State> {
+  state = {
+    page: ""
+  };
+
+  componentDidMount() {
+    const path = window.location.pathname;
+    const state = { page: path };
+
+    window.history.replaceState(state, path, path);
+    this.setState(state);
+
+    window.onpopstate = this.historyPopHandler;
+  }
+
+  componentWillUnmount() {
+    window.onpopstate = null;
+  }
+
+  historyPopHandler = event => {
+    const { state } = event;
+
+    if (state) {
+      this.setState(state);
+    }
+  };
+
+  changePage = path => {
+    window.history.pushState({ ...this.state, page: path }, path, path);
+    this.setState({ page: path });
+  };
+
+  navigation = () => (
+    <div className="list-row">
+      <button
+        className="text-button"
+        onClick={() => this.changePage("/profile")}
+      >
+        profile
+      </button>
+      <button
+        className="text-button"
+        onClick={() => this.changePage("/encouragements")}
+      >
+        encouragements
+      </button>
+    </div>
+  );
+
   render() {
-    return (
-      <div>
-        <RandomEncouragement />
-        <Profile {...this.props} />
-        <Encouragements />
-        <AddEncouragement />
-      </div>
-    );
+    const { page } = this.state;
+    const Navigation = this.navigation;
+
+    switch (page) {
+      case "/profile":
+        return (
+          <div>
+            <Navigation />
+            <RandomEncouragement />
+            <Profile {...this.props} />
+          </div>
+        );
+      case "/encouragements":
+        return (
+          <div>
+            <Navigation />
+            <RandomEncouragement />
+            <Encouragements />
+            <AddEncouragement />
+          </div>
+        );
+      case "/":
+      default:
+        return (
+          <div>
+            <Navigation />
+            <RandomEncouragement />
+          </div>
+        );
+    }
   }
 }
