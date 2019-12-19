7d3dc0191c549b3a7665e68be08c6f401d9a2318
chris <daiyitastic@gmail.com>
2019-12-18 18:12:12 -0800

    add dropdown for existing exercises consider using selection library to handle this

--- src/Workouts.tsx
+++ src/Workouts.tsx
@@ -1,8 +1,25 @@
 import React from "react";
-import { Exercise } from "./types";
+import { Exercise, Workout } from "./types";
+import { watchExercises } from "./Database";
 
 export default class Workouts extends React.PureComponent {
   state = { exercises: [] };
+
+  constructor(props) {
+    super(props);
+    watchExercises(this.updateExercises);
+  }
+
+  updateExercises = (newExercises: Array<Exercise>) => {
+    this.setState({ exercises: newExercises });
+  };
+
+  addExercise = async event => {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    // updateWorkout(form.get("name"), form.get("description"));
+    // event.target.reset();
+  };
 
   render() {
     const { exercises } = this.state;
@@ -21,6 +38,39 @@ export default class Workouts extends React.PureComponent {
             );
           })}
         </ul>
+        <form className="section" onSubmit={this.addExercise}>
+          <label>
+            Plan a new workout
+            <input
+              type="text"
+              name="name"
+              defaultValue=""
+              placeholder="leg day"
+            />
+            <input
+              type="text"
+              name="description"
+              defaultValue={""}
+              placeholder="workout description"
+            />
+          </label>
+
+          <label>
+            Add exercise
+            <select name="exercise" id="pet-select">
+              {exercises.map((exercise: Exercise, index) => {
+                return (
+                  <option key={index} value="">
+                    {exercise.name}
+                    {exercise.description && " - "}
+                    {exercise.description}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
+          <button type="submit">add</button>
+        </form>
       </div>
     );
   }
