632a3d98b7589d764ab74b5b4b9bca61579278d6
chris <daiyitastic@gmail.com>
2020-02-17 01:57:38 -0800

    create history singleton and SPA link component

---
+++ src/History.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { createBrowserHistory } from "history";
+
+// This creates a singleton that holds the context to
+// browser history. Import this to manipulate history,
+// including pushing and popping.
+// https://github.com/ReactTraining/history/tree/master/docs
+// If you want to navigate between pages, use the Link
+// component below.
+const history = createBrowserHistory();
+export default history;
+
+export const Link = ({
+  children,
+  href,
+  hideTextDecoration = false,
+  selected = false,
+  ...props
+}) => (
+  <button
+    className={`text-button 
+      ${hideTextDecoration ? "text-button-stealth" : ""} 
+      ${selected ? "text-button-selected" : ""} `}
+    onClick={() => history.push(href)}
+    {...props}
+  >
+    {children}
+  </button>
+);
